95cbbb16ebae593e4e82df376108820b3dad233f
renegarcia225 <renegabriel.gll@gmail.com>
2025-02-21 14:23:25 -0700

    Added supabase handler

--- src/api/ChatBotAPI.ts
+++ src/api/ChatBotAPI.ts
@@ -6,7 +6,7 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94cWxiYnlmb2Nlamlvb2Vpc2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNjk4NjEsImV4cCI6MjA1NTc0NTg2MX0.-CprkNuOtyaoanw2WJeq82c0NKyJHjOdS3FFDP1m-WY"
 );
 
-export default class ChatBotAPI {
+export default class SupabaseAPI {
   async postMessage() {
     const { data, error } = await supabase
       .from("messages")
