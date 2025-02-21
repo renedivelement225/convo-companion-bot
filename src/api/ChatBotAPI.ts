903486754ec1f32ef3cfcc96d54187689dff9097
renegarcia225 <renegabriel.gll@gmail.com>
2025-02-21 14:30:26 -0700

    Made supabase a private field

--- src/api/ChatBotAPI.ts
+++ src/api/ChatBotAPI.ts
@@ -1,14 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Add proper keys
-const supabase = createClient(
-  "https://oxqlbbyfocejiooeisip.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94cWxiYnlmb2Nlamlvb2Vpc2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNjk4NjEsImV4cCI6MjA1NTc0NTg2MX0.-CprkNuOtyaoanw2WJeq82c0NKyJHjOdS3FFDP1m-WY"
-);
+export default class ChatBotAPI {
+  #supabase; // Private class field
 
-export default class SupabaseAPI {
+  constructor() {
+    this.#supabase = createClient(
+      "https://<project>.supabase.co",
+      "<your-anon-key>"
+    );
+  }
+
   async postMessage() {
-    const { data, error } = await supabase
+    const { data, error } = await this.#supabase
       .from("messages")
       .insert([{ message: "Hello world" }]);
   }
